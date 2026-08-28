d432e27f575d9562124d56d06f7f0e042dbc8953
Nicu112 <nicusavary@hotmail.com>
2026-08-28 18:14:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
       standort: "Arolla, CH",
       ordner: "les-marmottes", // Unterordner in bilder/ und beschreibungen/
       googleCalendarId: "4272fb587c47328f819830065cf06566108ca68f64dcf0a6a89f6fd6f399b933@group.calendar.google.com",
-      appsScriptUrl: "https://script.google.com/macros/s/AKfycbyWEDd57BOQ44hfJeDVKOeYo0-ukzfHPMxlSGLVZWpmoK3j97kG1frZh8whYrWLe28T_g/exec",
+      appsScriptUrl: "https://script.google.com/macros/s/AKfycbyT5WaZH2aNdd5BrzImm1iq_FbX7HXxJ26JDIc6eplPeSzN2OALO3_cX4iUM3ct39JsfQ/exec",
       verwalterEmail: "lesmarmottesb@gmail.com",
       bilder: ["bilder/les-marmottes/1.jpg", "bilder/les-marmottes/2.jpg", "bilder/les-marmottes/3.jpg", "bilder/les-marmottes/4.jpg", "bilder/les-marmottes/5.jpg"]
     },
@@ -24,6 +24,15 @@ const CONFIG = {
       appsScriptUrl: "",
       verwalterEmail: "lesdeuxcypres5@gmail.com",
       bilder: ["bilder/les-deux-cypres/1.jpg", "bilder/les-deux-cypres/2.jpg", "bilder/les-deux-cypres/3.jpg", "bilder/les-deux-cypres/4.jpg", "bilder/les-deux-cypres/5.jpg"]
+    },
+    haus3: {
+      name: "Orion",
+      standort: "Leytron, CH",
+      ordner: "Ovronnaz",
+      googleCalendarId: "11d1b8e4cccbbed74089b37945fba45f7d5afec1c700ad80fa575bfd86b0d051@group.calendar.google.com",
+      appsScriptUrl: "https://script.google.com/macros/s/AKfycby3C8YIJY1hhqLV8bHBUWaaGt3QNKFi1zB2CQdOBktflTy0YHQIi0dMWZBsmuSi2Usgtw/exec",
+      verwalterEmail: "ovz.orion@gmail.com",
+      bilder: ["bilder/Ovronnaz/1.jpg", "bilder/Ovronnaz/2.jpg", "bilder/Ovronnaz/3.jpg", "bilder/Ovronnaz/4.jpg", "bilder/Ovronnaz/5.jpg"]
     }
   }
 };
